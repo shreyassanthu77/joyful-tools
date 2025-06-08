7c455d342d7092ea5e9fc13f8da7206997e078f0
Shreyas Mididoddi <me@shreyascodes.tech>
2025-06-08 17:36:15 +0530

    remove kv and queue stuff for now

--- src/lib/server/db/email.ts
+++
@@ -1,9 +0,0 @@
-import { table,  defaults, text, json } from "./shared";
-
-export const emails = table("emails", (t) => ({
-	...defaults,
-	subject: text(),
-	to: json<string[]>(),
-	from: text(),
-	bodyHtml: text(),
-}));
--- src/lib/server/kv.ts
+++
@@ -1,4 +0,0 @@
-import { openKv } from "@deno/kv";
-import { DENO_KV_URL } from "$env/static/private";
-
-export const kv = await openKv(DENO_KV_URL);
