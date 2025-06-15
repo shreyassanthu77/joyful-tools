269385be000d23bc54fa7742619f8deb99893e57
Shreyas Mididoddi <me@shreyascodes.tech>
2025-06-15 05:44:30 +0530

    chore(template): make deno kv work with bun

--- template/src/lib/server/kv.ts
+++ template/src/lib/server/kv.ts
@@ -1,7 +1,12 @@
 import { createKv } from "@joyful/kv";
 import { openKv, createDenoDriver } from "@joyful/kv/deno-kv-node";
+import { serialize as encodeV8, deserialize as decodeV8 } from "node:v8";
 
-const denoKv = await openKv();
+const denoKv = await openKv("kv.db", {
+	implementation: "sqlite",
+	encodeV8,
+	decodeV8,
+});
 
 export const kv = createKv({
 	driver: createDenoDriver({
