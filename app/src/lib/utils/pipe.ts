fbe1e7625724005ebadb729a48523bf3c43f76c9
Shreyas Mididoddi <me@shreyascodes.tech>
2025-11-16 18:25:50 +0530

    app: moar pipe

--- app/src/lib/utils/pipe.ts
+++ app/src/lib/utils/pipe.ts
@@ -76,6 +76,86 @@ export function pipe<T, U, V, W, X, Y, Z, A, B, C, D>(
 	n: (value: B) => C,
 	o: (value: C) => D
 ): D;
+export function pipe<T, U, V, W, X, Y, Z, A, B, C, D, E>(
+	value: T,
+	fn: (value: T) => U,
+	g: (value: U) => V,
+	h: (value: V) => W,
+	i: (value: W) => X,
+	j: (value: X) => Y,
+	k: (value: Y) => Z,
+	l: (value: Z) => A,
+	m: (value: A) => B,
+	n: (value: B) => C,
+	o: (value: C) => D,
+	p: (value: D) => E
+): E;
+export function pipe<T, U, V, W, X, Y, Z, A, B, C, D, E, F>(
+	value: T,
+	fn: (value: T) => U,
+	g: (value: U) => V,
+	h: (value: V) => W,
+	i: (value: W) => X,
+	j: (value: X) => Y,
+	k: (value: Y) => Z,
+	l: (value: Z) => A,
+	m: (value: A) => B,
+	n: (value: B) => C,
+	o: (value: C) => D,
+	p: (value: D) => E,
+	q: (value: E) => F
+): F;
+export function pipe<T, U, V, W, X, Y, Z, A, B, C, D, E, F, G>(
+	value: T,
+	fn: (value: T) => U,
+	g: (value: U) => V,
+	h: (value: V) => W,
+	i: (value: W) => X,
+	j: (value: X) => Y,
+	k: (value: Y) => Z,
+	l: (value: Z) => A,
+	m: (value: A) => B,
+	n: (value: B) => C,
+	o: (value: C) => D,
+	p: (value: D) => E,
+	q: (value: E) => F,
+	r: (value: F) => G
+): G;
+export function pipe<T, U, V, W, X, Y, Z, A, B, C, D, E, F, G, H>(
+	value: T,
+	fn: (value: T) => U,
+	g: (value: U) => V,
+	h: (value: V) => W,
+	i: (value: W) => X,
+	j: (value: X) => Y,
+	k: (value: Y) => Z,
+	l: (value: Z) => A,
+	m: (value: A) => B,
+	n: (value: B) => C,
+	o: (value: C) => D,
+	p: (value: D) => E,
+	q: (value: E) => F,
+	r: (value: F) => G,
+	s: (value: G) => H
+): H;
+export function pipe<T, U, V, W, X, Y, Z, A, B, C, D, E, F, G, H, I>(
+	value: T,
+	fn: (value: T) => U,
+	g: (value: U) => V,
+	h: (value: V) => W,
+	i: (value: W) => X,
+	j: (value: X) => Y,
+	k: (value: Y) => Z,
+	l: (value: Z) => A,
+	m: (value: A) => B,
+	n: (value: B) => C,
+	o: (value: C) => D,
+	p: (value: D) => E,
+	q: (value: E) => F,
+	r: (value: F) => G,
+	s: (value: G) => H,
+	t: (value: H) => I
+): I;
 export function pipe<T, const Rest extends readonly ((a: any) => any)[]>(value: T, ...fns: Rest) {
 	let result = value;
 	for (const fn of fns) {
