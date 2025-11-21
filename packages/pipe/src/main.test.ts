2877b6bb4f2ef42fd139318a11d96ff785cf712d
Shreyas Mididoddi <me@shreyascodes.tech>
2025-11-21 21:32:18 +0530

    feat(pipe): replace vitest with deno test

--- packages/pipe/src/main.test.ts
+++ packages/pipe/src/main.test.ts
@@ -1,13 +1,11 @@
 import { pipe } from "@joyful/pipe";
-import { describe, expect, it } from "vitest";
+import { assertEquals } from "assert";
 
 const double = (value: number) => value * 2;
 const square = (value: number) => value * value;
 const add = (x: number) => (value: number) => value + x;
 
-describe("pipe", () => {
-  it("should work", () => {
-    const result = pipe(4, double, square, add(5));
-    expect(result).toBe(69);
-  });
+Deno.test("pipe", () => {
+  const result = pipe(4, double, square, add(5));
+  assertEquals(result, 69);
 });
