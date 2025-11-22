f3e85ee86065af355245754ee98a7a2b11147517
Shreyas Mididoddi <me@shreyascodes.tech>
2025-11-22 14:28:20 +0530

    fix(option): tests

--- packages/option/src/option.test.ts
+++ packages/option/src/option.test.ts
@@ -1,18 +1,10 @@
 import { assert, assertEquals, assertThrows } from "assert";
 import { pipe } from "@joyful/pipe";
-import {
-  andThen,
-  fromNullable,
-  map,
-  match,
-  None,
-  orElse,
-  Some,
-} from "@joyful/option";
+import { Option } from "@joyful/option";
 
 Deno.test("Option", async (t) => {
   await t.step("Some behaves correctly", () => {
-    const s = new Some(10);
+    const s = new Option.Some(10);
     assert(s.isSome());
     assert(!s.isNone());
     assertEquals(s.unwrap(), 10);
@@ -20,7 +12,7 @@ Deno.test("Option", async (t) => {
   });
 
   await t.step("None behaves correctly", () => {
-    const n = None;
+    const n = Option.None;
     assert(!n.isSome());
     assert(n.isNone());
     assertThrows(() => n.unwrap(), Error, "called `unwrap` on a `None` value");
@@ -28,80 +20,80 @@ Deno.test("Option", async (t) => {
   });
 
   await t.step("fromNullable behaves correctly", () => {
-    const s = fromNullable(10);
+    const s = Option.fromNullable(10);
     assert(s.isSome());
     assertEquals(s.unwrap(), 10);
 
-    const n1 = fromNullable(null);
+    const n1 = Option.fromNullable(null);
     assert(n1.isNone());
 
-    const n2 = fromNullable(undefined);
+    const n2 = Option.fromNullable(undefined);
     assert(n2.isNone());
   });
 
   await t.step("map behaves correctly", () => {
-    const s = new Some(10);
-    const m = map(s, (x) => x * 2);
+    const s = new Option.Some(10);
+    const m = Option.map(s, (x) => x * 2);
     assert(m.isSome());
     assertEquals(m.unwrap(), 20);
 
-    const n = None;
-    const m2 = map(n, (x: number) => x * 2);
+    const n = Option.None;
+    const m2 = Option.map(n, (x: number) => x * 2);
     assert(m2.isNone());
 
     // Curried
-    const doubler = map((x: number) => x * 2);
+    const doubler = Option.map((x: number) => x * 2);
     const m3 = doubler(s);
     assertEquals(m3.unwrap(), 20);
   });
 
   await t.step("andThen behaves correctly", () => {
-    const s = new Some(10);
-    const m = andThen(s, (x) => new Some(x * 2));
+    const s = new Option.Some(10);
+    const m = Option.andThen(s, (x) => new Option.Some(x * 2));
     assert(m.isSome());
     assertEquals(m.unwrap(), 20);
 
-    const n = None;
-    const m2 = andThen(n, (x: number) => new Some(x * 2));
+    const n = Option.None;
+    const m2 = Option.andThen(n, (x: number) => new Option.Some(x * 2));
     assert(m2.isNone());
 
-    const m3 = andThen(s, (_: number) => None);
+    const m3 = Option.andThen(s, (_: number) => Option.None);
     assert(m3.isNone());
 
     // Curried
-    const doubler = andThen((x: number) => new Some(x * 2));
+    const doubler = Option.andThen((x: number) => new Option.Some(x * 2));
     const m4 = doubler(s);
     assertEquals(m4.unwrap(), 20);
   });
 
   await t.step("orElse behaves correctly", () => {
-    const s = new Some(10);
-    const m = orElse(s, () => new Some(20));
+    const s = new Option.Some(10);
+    const m = Option.orElse(s, () => new Option.Some(20));
     assert(m.isSome());
     assertEquals(m.unwrap(), 10);
 
-    const n = None;
-    const m2 = orElse(n, () => new Some(20));
+    const n = Option.None;
+    const m2 = Option.orElse(n, () => new Option.Some(20));
     assert(m2.isSome());
     assertEquals(m2.unwrap(), 20);
 
     // Curried
-    const defaulter = orElse(() => new Some(20));
+    const defaulter = Option.orElse(() => new Option.Some(20));
     const m3 = defaulter(n);
     assertEquals(m3.unwrap(), 20);
   });
 
   await t.step("match behaves correctly", () => {
-    const s = new Some(10);
-    const m = match(
+    const s = new Option.Some(10);
+    const m = Option.match(
       s,
       (x) => `Got ${x}`,
       () => "Got nothing",
     );
     assertEquals(m, "Got 10");
 
-    const n = None;
-    const m2 = match(
+    const n = Option.None;
+    const m2 = Option.match(
       n,
       (x: number) => `Got ${x}`,
       () => "Got nothing",
@@ -109,7 +101,7 @@ Deno.test("Option", async (t) => {
     assertEquals(m2, "Got nothing");
 
     // Curried
-    const matcher = match(
+    const matcher = Option.match(
       (x: number) => `Got ${x}`,
       () => "Got nothing",
     );
@@ -118,8 +110,8 @@ Deno.test("Option", async (t) => {
 
   await t.step("works with pipe", () => {
     const result = pipe(
-      new Some(5),
-      map((x) => x * 2),
+      new Option.Some(5),
+      Option.map((x) => x * 2),
       (opt) => opt.unwrapOr(0),
     );
     assertEquals(result, 10);
