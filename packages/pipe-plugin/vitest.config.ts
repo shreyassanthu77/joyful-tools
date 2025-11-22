9f3dfa7d404a1de79b81c88aa7670b443e8ce8de
Shreyas Mididoddi <me@shreyascodes.tech>
2025-11-22 06:37:09 +0530

    sadge(pipe-plugin): no more

--- packages/pipe-plugin/vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vitest/config";
-import deno from "@deno/vite-plugin";
-import { pipePlugin } from "./src/mod.ts";
-
-export default defineConfig({
-  plugins: [deno(), pipePlugin()],
-});
